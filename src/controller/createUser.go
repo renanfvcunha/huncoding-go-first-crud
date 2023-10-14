5dc8eece09ec0d61d96ba91843a621139839ad31
Renan Cunha <rfvcunha@hotmail.com>
2023-10-14 09:51:20 -0300

    feat: add logs on project

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,23 +1,36 @@
 package controller
 
 import (
-	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/renanfvcunha/huncoding-go-first-crud/src/config/logger"
 	"github.com/renanfvcunha/huncoding-go-first-crud/src/config/validation"
 	"github.com/renanfvcunha/huncoding-go-first-crud/src/controller/model/request"
+	"github.com/renanfvcunha/huncoding-go-first-crud/src/controller/model/response"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Init CreateUser Controller", zap.String("journey", "createUser"))
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Error trying to validate user info", err, zap.String("journey", "createUser"))
 		restErr := validation.ValidateUserError(err)
 
 		c.JSON(restErr.Code, restErr)
-
 		return
 	}
 
-	fmt.Println(userRequest)
+	response := response.UserResponse{
+		ID:    "test",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+
+	logger.Info("User created successfully", zap.String("journey", "createUser"))
+
+	c.JSON(http.StatusOK, response)
 }
