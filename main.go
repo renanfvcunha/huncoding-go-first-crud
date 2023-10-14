0aa7f49e7fc54f8af0325d58fa453191d23b8b18
Renan Cunha <rfvcunha@hotmail.com>
2023-10-14 12:36:16 -0300

    feat: create database constructor

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"github.com/renanfvcunha/huncoding-go-first-crud/src/config/database/mongodb"
 	"github.com/renanfvcunha/huncoding-go-first-crud/src/config/logger"
 	"github.com/renanfvcunha/huncoding-go-first-crud/src/controller"
 	"github.com/renanfvcunha/huncoding-go-first-crud/src/controller/routes"
@@ -19,8 +18,6 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
-	mongodb.InitConnection()
 
 	// Init Deps
 	service := service.NewUserDomainService()
--- src/config/database/mongodb/mongodbConnection.go
+++ src/config/database/mongodb/mongodbConnection.go
@@ -2,24 +2,33 @@ package mongodb
 
 import (
 	"context"
+	"os"
 
 	"github.com/renanfvcunha/huncoding-go-first-crud/src/config/logger"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func InitConnection() {
-	ctx := context.Background()
+var (
+	MONGODB_URL      = "MONGODB_URL"
+	MONGODB_DATABASE = "MONGODB_DATABASE"
+)
 
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017/huncodingCrudInit"))
+func NewMongoDbConnection(ctx context.Context) (*mongo.Database, error) {
+	mongodbUri := os.Getenv(MONGODB_URL)
+	mongodbDatabase := os.Getenv(MONGODB_DATABASE)
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongodbUri))
 
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	if err := client.Ping(ctx, nil); err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	logger.Info("Database Connected Successfully")
+
+	return client.Database(mongodbDatabase), nil
 }
